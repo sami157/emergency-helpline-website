e47a0b6b4ec4e318f92828fc4310bf34e4ed1db7
sami157 <sami.eee157@gmail.com>
2025-08-29 13:23:23 +0600

    finished (probably)

--- dom-code.js
+++ dom-code.js
@@ -5,9 +5,11 @@ let heartCount = heartCountInitial
 let coinCount = coinCountInitial
 let copyCount = copyCountInitial
 let canCall = true
+
 document.getElementById('heart-count').innerText = heartCountInitial
 document.getElementById('coin-count').innerText = coinCountInitial
 document.getElementById('copy-count').innerText = copyCountInitial + ' Copy'
+
 function processClick(
     inputClass,
     count,
@@ -16,23 +18,25 @@ function processClick(
     hasCountLimit,
     countLimitType,
     countLimit,
-    countLimitMessage,
     extraText,
     clickMessage
  ){
     for (let target of document.getElementsByClassName(inputClass)){
     target.addEventListener('click', function(event){
+        if(inputClass==='clear-button'){
+                document.getElementById('call-history-container').children[1].innerHTML = ''
+                console.log('I am clicked')
+        }
+        else{
         if(hasCountLimit){
             if(countLimitType==='u'){
                 if(count>=countLimit){
-                    console,log('Limit Over')
                     canCall = false;
                 }
                 else count = count + countStep
             }
             if(countLimitType==='l'){
                 if(count<=countLimit){
-                    console.log('Limit Under')
                     canCall = false;
                 }
                 else count = count + countStep
@@ -54,19 +58,20 @@ function processClick(
                     let listItem = document.createElement('div')
                     listItem.classList.add("text-[18px]", "rounded-[12px]", "p-[16px]", "flex", "justify-between", "bg-[#FAFAFA]")
                     listItem.innerHTML = '<div class="history-card-left gap-[4px] items-center"><p>'+ callName+'</p><p>'+callNumber+'</p></div><div class="flex items-center copy-count-text">'+ time + '</div>'
-                    console.log(listItem)
                     listItemContainer.appendChild(listItem)
                     alert('Calling '+ callName + ' ' + '('+ callNumber + ')')
                 }
             }
             else if(inputClass==='copy-button'){
-                navigator.clipboard.writeText(callNumber);
+                navigator.clipboard.writeText(callNumber)
                 alert(callNumber + ' copied to clipboard')
             }
         }
-        })
+        }})
     }
 }
-processClick('fa-heart',heartCount,'heart-count',1,false,'',0,'','','');
-processClick('call-button',coinCount,'coin-count',-20,true,'l',0,'','',);
-processClick('copy-button',copyCount,'copy-count',1,false,'',0,'',' Copy');
+
+processClick('fa-heart',heartCount,'heart-count',1,false,'',0,'','')
+processClick('call-button',coinCount,'coin-count',-20,true,'l',0,'','')
+processClick('copy-button',copyCount,'copy-count',1,false,'',0,' Copy','')
+processClick('clear-button')
